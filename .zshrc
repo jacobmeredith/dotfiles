37f33491184e9124d3775745475fa3f6e1018a78
jacobmeredith <jacobmeredith@hotmail.co.uk>
2024-01-06 00:02:30 +0000

    make sure files are commited

--- .zshrc
+++ .zshrc
@@ -1,10 +1,25 @@
 export ZSH="$HOME/.oh-my-zsh"
 
-plugins=(git zsh-nvm zsh-autosuggestions zsh-syntax-highlighting)
+export NVM_DIR=~/.nvm
+
+export GOPATH=$HOME/go
+export GOROOT=/opt/homebrew/opt/go/libexec
+
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOROOT/bin
+
+export PATH=/opt/homebrew/bin:$PATH:$GOPATH
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+
+plugins=(git zsh-syntax-highlighting)
 
 ZSH_THEME="gruvbox"
 SOLARIZED_THEME="dark"
 
 source $ZSH/oh-my-zsh.sh
+source $(brew --prefix nvm)/nvm.sh
 
 alias vim=nvim
--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -31,12 +31,14 @@ vim.keymap.set("n", "<leader>/", function()
 end, { desc = "[/] Fuzzily search in current buffer" })
 
 -- Use <space>gf to search in git files
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
 -- Use <space>sf to search files
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
 -- Use <space>sf to search help
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
 -- Use <space>sw to search current word
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
 -- Use <space>sw to search by grep
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+
+vim.keymap.set("n", "<leader>gs", require("telescope.builtin").git_status, { desc = "[Git] [S]tatus" })
