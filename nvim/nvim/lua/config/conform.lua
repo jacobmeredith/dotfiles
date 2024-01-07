a9b2d436973b04224b42ecfdebf33dce2dcb02ef
jacobmeredith <jacobmeredith@hotmail.co.uk>
2024-01-07 18:15:38 +0000

    use conform for format on save, remove manual format on save

--- nvim/nvim/lua/config/conform.lua
+++ nvim/nvim/lua/config/conform.lua
@@ -1,9 +1,13 @@
 require("conform").setup({
-  lsp_fallback = true,
+  format_on_save = {
+    lsp_fallback = true,
+  },
   formatters_by_ft = {
     lua = { "stylua" },
     javascript = { "prettier", "eslint_d" },
+    javascriptreact = { "prettier", "eslint_d" },
     typescript = { "prettier", "eslint_d" },
+    typescriptreact = { "prettier", "eslint_d" },
     css = { "prettier" },
     html = { "prettier" },
     go = { "gopls" },
