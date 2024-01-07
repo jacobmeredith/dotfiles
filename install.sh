ccda2daf038de280874d1667c2a2ff58e9101e90
jacobmeredith <jacobmeredith@hotmail.co.uk>
2024-01-07 15:24:50 +0000

    use stow

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+stow -t ~ -S zsh
+stow -t ~/.config -S nvim
+stow -t ~/.config -S tmux
+stow -t ~/.config -S alacritty
---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+stow -t ~ -D zsh
+stow -t ~/.config -D nvim
+stow -t ~/.config -D tmux
+stow -t ~/.config -D alacritty
