bccebc17e88e04421a4e4ad05622fbb73d34212e
jacobmeredith <jacobmeredith@hotmail.co.uk>
2024-01-12 13:34:01 +0000

    update lualine config

--- nvim/nvim/lua/config/lualine.lua
+++ nvim/nvim/lua/config/lualine.lua
@@ -3,5 +3,15 @@ local custom_catppuccin = require "lualine.themes.catppuccin"
 custom_catppuccin.normal.c.bg = "#262626"
 
 require("lualine").setup {
-  options = { theme = custom_catppuccin }
+  options = {
+    theme = custom_catppuccin,
+  },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        path = 1,
+      }
+    }
+  }
 }
